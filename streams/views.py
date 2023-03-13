fe76f7d3f525a350726e999b9a39f0bbd290dabe
KristianColville1 <kristiancolville1@gmail.com>
2023-03-13 17:22:31 +0000

    Added views, urls and templates. Fixtures added

--- streams/views.py
+++ streams/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Stream
 
-# Create your views here.
+
+def get_streams(request):
+    all_streams = Stream.objects.all()
+    context = {
+        'streams': all_streams
+    }
+    return render(request, 'streams/index.html', context)
