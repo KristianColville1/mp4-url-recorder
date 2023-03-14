d256f86ea3dc2e88091f07d0ca9c880986e8f736
KristianColville1 <kristiancolville1@gmail.com>
2023-03-14 14:35:33 +0000

    Update urls and views for streams app

--- streams/urls.py
+++ streams/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.get_streams, name='streams'),
+  path('<stream_name>/', views.get_stream_detail, name='stream_detail'),
 ]
--- streams/views.py
+++ streams/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Stream
 from django.contrib import messages
 from django.db.models import Q
+from .stream_calander import get_calander_days_of_month, DAYS
+
 
 def get_streams(request):
+    """
+    Gets all the streams and shows them in the front end.
+    If the request method is GET it will search the stream
+    objects for the query selected by a user.
+    """
     all_streams = Stream.objects.all()
     query = None
     
@@ -17,7 +24,29 @@ def get_streams(request):
 
             queries = Q(name__icontains=query)
             all_streams = all_streams.filter(queries)
+    all_streams = all_streams.order_by('name')
     context = {
         'streams': all_streams
     }
     return render(request, 'streams/index.html', context)
+
+
+def get_stream_detail(request, stream_name):
+    """
+    Gets the details of the stream selected in the front end
+    """
+    stream = Stream.objects.filter(name=stream_name)
+    stream = stream.last()
+    
+    previous_month = get_calander_days_of_month(2,2023)
+    present_month = get_calander_days_of_month(3, 2023)
+    
+    context = {
+        'stream': stream,
+        'previous_month_dict': previous_month,
+        'present_month_dict': present_month,
+        'this_month': 'March',
+        'last_month': 'February',
+        'days': DAYS,
+    }
+    return render(request, 'streams/stream_detail.html', context)
